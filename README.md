51b8f404ada65d67bb214f8dd084422029986387
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2023-01-10 10:56:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringSecurityBasics
+ Aqui um projeto de API onde eu exercito conhecimentos básicos de SpringSecurity
---
+++ src/main/java/com/workshop/fernanda/GreetingsController.java
@@ -0,0 +1,26 @@
+package com.workshop.fernanda;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Fernanda S.
+ */
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingsController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello! diz a API! :) ");
+    }
+
+
+    @GetMapping("/say-good-bye")
+    public  ResponseEntity<String> sayGoodbye() {
+        return ResponseEntity.ok("Goodbye! Até Logo :) ");
+    }
+}
