35587683aac718229048373df8dd655b681f7850
Fernanda Melo <89407722+Fernandademelo@users.noreply.github.com>
2023-01-15 00:05:37 -0300

    Update jjwt

--- src/main/java/com/workshop/fernanda/config/JwtAthFilter.java
+++ src/main/java/com/workshop/fernanda/config/JwtAthFilter.java
@@ -1,9 +1,44 @@
 package com.workshop.fernanda.config;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.http.parser.Authorization;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-//extends a filter class para que cada request sea interceptada por esse filtro primeiro
+import java.io.IOException;
 
+//extends a filter class para que cada request sea interceptada por esse filtro primeiro
+@Component
 public class JwtAthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader(Authorization);
+        final String userEmail;
+        final String jwtToken;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        userEmail = "something"; // TODO to be implemented
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userDetailsService.loasUserByUsername(userEmail);
+            final boolean isTokenValid; // TODO to be implemented
+            if (isTokenValid) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities())
+            }
+        }
+    }
 
 }
